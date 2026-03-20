24011af37eddcf11a4f536135ffeb52876acd85d
K. <k@example.com>
2026-03-20 14:10:15 -0500

    Local names

---
+++ .prereqs/AA-tf/modules/github/orgconfig/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  my_gh_repo_b_name = "${var.workload_nickname}repob"
+  my_gh_app_slug = "${var.workload_nickname}gha"
+  my_gh_org_secret_name = "${var.workload_nickname}os"
+  my_gh_repo_a_name = "${var.workload_nickname}repoa"
+}
